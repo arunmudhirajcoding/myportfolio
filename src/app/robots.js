bfc91312ec00344b8d793c136a2378ff113e3d7e
arunmudhirajcoding <duruguarun@gmail.com>
2026-07-19 01:15:18 +0530

    robots and sitemap added

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://arun-kumar-durgollu.vercel.app/sitemap.xml",
+  };
+}
